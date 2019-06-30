b2e58d5a02a00e29062b11c3b14a9e0c91009006
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-06-30 13:12:14 +0600

    making model changes

--- srvup/src/videos/models.py
+++ srvup/src/videos/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 
 class Video(models.Model):
+	title      = models.CharField(max_length=120)
 	embed_code = models.TextField()
+	timestamp  = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.embed_code  
