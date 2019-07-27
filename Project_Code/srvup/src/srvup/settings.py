0640c4b2efc14488da650961ea81475fabb10591
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-07-28 03:03:59 +0600

    feature #20 : settings and urls

--- Project_Code/srvup/src/srvup/settings.py
+++ Project_Code/srvup/src/srvup/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'videos',
+
+    'memberships',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR), 'static_root'],
+
+VENV_PATH = os.path.dirname(BASE_DIR)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+
+
+
+
+
+
+
+
