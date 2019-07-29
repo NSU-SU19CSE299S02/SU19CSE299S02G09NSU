d72418f7d1519370c633db275f85f32f02f7493d
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-07-30 05:22:51 +0600

    Feature #28 : membership subscription

--- Project_Code/webjax/Code/src/memberships/models.py
+++ Project_Code/webjax/Code/src/memberships/models.py
@@ -1,4 +1,8 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+
+import stripe
 
 # Create your models here.
 
@@ -19,4 +23,36 @@ class Membership(models.Model):
 
 	def __str__(self):
 		return self.membership_type
+
+class Usermembership(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	stripe_plan_id = models.CharField(max_length=40)
+	membership = models.ForeignKey(Membership, on_delete=models.SET_NULL, null=True)
+
+def post_save_usermembership_create(sender, instance, created, *args, **kwargs):
+	if created:
+		Usermembership.objects.get_or_create(user=instance)
+
+	user_membership, created= Usermembership.objects.get_or_create(user=instance)
+
+	if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id == '':
+		new_customer_id = stripe..Customer.create(email=instance.email)
+		user_membership.stripe_customer_id = new_customer_id['id']
+		user_membership.save()
+
+post_save.connect(post_save_usermembership_create, sender=settings.AUTH_USER_MODEL)
+
+
+class Subscription(models.Model):
+	user_membership = models.ForeignKey(Usermembership, on_delete=models.CASCADE)
+	stripe_subscription_id = models.CharField(max_length=40)
+	active = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.user_membership.user.username
+
+
+
+
+
 
