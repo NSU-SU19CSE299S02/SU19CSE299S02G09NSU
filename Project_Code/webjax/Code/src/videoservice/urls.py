2d2f69781aba42fd992887294519746e3eaf9bb1
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-07-30 03:10:44 +0600

    Feature #25 : webjax view

--- Project_Code/webjax/Code/src/videoservice/urls.py
+++ Project_Code/webjax/Code/src/videoservice/urls.py
@@ -4,18 +4,9 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-"""
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
-"""
---
+++ Project_Code/webjax/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alvee32/NSU/SU19CSE299S02G09NSU/Project_Code/webjax/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
