301ad7885898574de2a93dd6e71a78a1e459a474
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-08-06 08:02:02 +0600

    feature #32 : lession field

--- Project_Code/webjax/Code/src/courses/models.py
+++ Project_Code/webjax/Code/src/courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from membership.models import Membership
 # Create your models here.
 
 class Course(models.Model):
@@ -7,6 +7,17 @@ class Course(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextFiled()
 	allowed_memberships = models.ManyToManyField(Membership)
+
+	def __str__(self):
+		return self.title
+
+class Lesson(models.Model):
+	slug = models.SlugField()
+	title = models.CharField(max_length=120)
+	course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+	position = models.IntegerField()
+	video_url = models.CharField(max_length=200)
+	thumbnail = models.ImageField()
 
 	def __str__(self):
 		return self.title
