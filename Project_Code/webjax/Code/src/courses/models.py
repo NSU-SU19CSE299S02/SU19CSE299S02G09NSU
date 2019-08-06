6fa753c1735dbbad88bb38d66471e38ef2d134ab
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-08-06 07:56:51 +0600

    feature #31 : course field

--- Project_Code/webjax/Code/src/courses/models.py
+++ Project_Code/webjax/Code/src/courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+	slug = models.SlugField()
+	title = models.CharField(max_length=120)
+	description = models.TextFiled()
+	allowed_memberships = models.ManyToManyField(Membership)
+
+	def __str__(self):
+		return self.title
