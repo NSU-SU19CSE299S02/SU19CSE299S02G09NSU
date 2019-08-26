5d0267ff6d0e0123e8731b1cad87c06c7e05b4f1
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-08-27 02:19:35 +0600

    feature #41 : first lesson view

--- Project_Code/webjax/Code/src/courses/models.py
+++ Project_Code/webjax/Code/src/courses/models.py
@@ -15,6 +15,11 @@ class Course(models.Model):
 	def get_absolute_url(self):
 		return reverse('courses:detail', kwargs={'slug': self.slug})
 
+	@property
+	def lessons(self):
+		return self.lesson_set.all().order_by('position')
+
+
 
 class Lesson(models.Model):
 	slug = models.SlugField()
@@ -26,3 +31,21 @@ class Lesson(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('courses:lesson-detail',
+			kwargs={
+			'course_slug': self.course.slug,
+			'lesson_slug': self.slug
+			})
+
+
+
+
+
+
+
+
+
+
+
