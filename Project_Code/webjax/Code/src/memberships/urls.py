21b240dc66d1b96c59d76d8e45bcc80ea241e0cf
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-08-27 08:37:20 +0600

    feature #54 : update transaction

--- Project_Code/webjax/Code/src/memberships/urls.py
+++ Project_Code/webjax/Code/src/memberships/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MembershipSelectView, PaymentView
+from .views import MembershipSelectView, PaymentView, updateTransactionsRecords
 
 app_name = 'memberships'
 
 urlpatterns = [
 	path('', MembershipSelectView.as_view(), name='select'),
-	path('payment', PaymentView, name='payment')
+	path('payment/', PaymentView, name='payment'),
+	path('update-transactions/<subscription_id>/', updateTransactionsRecords, name='update-transactions')
 	 ]
