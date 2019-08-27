686772c538a736c4fb7a0b6927ce336909bb48f3
Ashraful Islam Alvee <alvmz32@gmail.com>
2019-08-27 09:11:53 +0600

    feature #55 : profile view added

--- Project_Code/webjax/Code/src/memberships/urls.py
+++ Project_Code/webjax/Code/src/memberships/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import MembershipSelectView, PaymentView, updateTransactionsRecords
+from .views import (
+    MembershipSelectView,
+    PaymentView,
+    updateTransactionRecords,
+    profile_view,
+    )
 
 app_name = 'memberships'
 
 urlpatterns = [
-	path('', MembershipSelectView.as_view(), name='select'),
-	path('payment/', PaymentView, name='payment'),
-	path('update-transactions/<subscription_id>/', updateTransactionsRecords, name='update-transactions')
-	 ]
+    path('', MembershipSelectView.as_view(), name='select'),
+    path('payment/', PaymentView, name='payment'),
+    path('update-transactions/<subscription_id>/',
+         updateTransactionRecords, name='update-transactions'),
+    path('profile/', profile_view, name='profile'),
+    ]
